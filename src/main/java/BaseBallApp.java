29ce63ce591062dd851b14323055589a1775de0f
Merhong <zx2938@gmail.com>
2023-07-12 15:50:52 +0900

    1. PlayerDao 완성 2. BaseBallApp에서 PlayerDao 호출

--- src/main/java/BaseBallApp.java
+++ src/main/java/BaseBallApp.java
@@ -8,6 +8,8 @@ import java.sql.Connection;
 import java.util.List;
 import java.util.Scanner;
 
+import static java.time.LocalTime.now;
+
 public class BaseBallApp {
     public static void main(String[] args) {
         // DB 연결 (baseballdb)
@@ -28,6 +30,8 @@ public class BaseBallApp {
         String input = sc.nextLine();         // 클라이언트로부터 키보드 입력을 받는다.
         inputParsing = input.split("\\?");
 
+
+
         // 2.1.3 main메소드에서 할 일 (서비스 구현)
         // 입력받은 값(input) : 선수등록?teamId=1&name=이대호&position=1루수
         // "?"로 파싱하면 [0] = 선수등록, [1] = teamId=1&name=이대호&position=1루수
@@ -44,7 +48,34 @@ public class BaseBallApp {
         // 3.4 전체 팀 목록
 
         // 3.5 선수 등록
+        if (inputParsing[0].equals("선수등록")) {
+            // input : 선수등록?teamId=1&name=이대호&position=1루수
+            // inputParsing : "?"를 기준으로 [0]선수등록 [1]teamId=1&name=이대호&position=1루수
+            // temp     : "&" 기준으로 다시 split :  [0]teamId=1, [1]name=이대호, [2]position=1루수
+            // temp2    : "=" 기준으로 다시 split :  [0]teamId, [1]1, [2]name, [3]이대호, [4]position, [5]1루수
+            // temp2에서 분할시 인덱스 오류 발생! 반복문을 사용하여 split 후에  [0],[1]에 할당해줘야 함
+            String[] temp = inputParsing[1].split("&");
+            String[] temp2 = new String[temp.length * 2];     // temp2[0] = teamId , temp2[1] = 1
 
+            // temp2의 스플릿을 여러번 하기 위해 반복문 사용하여 값 할당.
+            for (int i = 0; i < temp.length; i++) {
+                String[] pair = temp[i].split("="); // i=1 : pair[0] = teamId / pair[1] = 1
+                temp2[i * 2] = pair[0];
+                temp2[i * 2 + 1] = pair[1];
+            }
+
+            try {
+                List<Player> players = playerDao.registerPlayer(
+                        Integer.parseInt(temp2[1]),             // teamId (int)
+                        temp2[3],   // name
+                        temp2[5]    // position
+                );
+                System.out.println("선수등록 성공");
+            } catch (Exception e) {
+                System.out.println("선수등록 실패");
+                e.printStackTrace();
+            }
+        }
 
         // 3.6 팀별 선수 목록
         if (inputParsing[0].equals("선수목록")) {
@@ -54,7 +85,7 @@ public class BaseBallApp {
             try {
                 List<Player> players = playerDao.getTeamPlayers(
                         Integer.parseInt(inputParsing[1].split("=")[1])  // getTeamPlayers( Index ) 넣는 부분, 정수가 필요하다.
-                                                                                // [1][1]인 문자열 1을 정수 1로 바꿔서 동적으로 할당 가능하게 함.
+                        // [1][1]인 문자열 1을 정수 1로 바꿔서 동적으로 할당 가능하게 함.
                 );
                 System.out.println("선수목록 출력");
                 System.out.println("=================");
